0b01f7d8c277cf0cdc12d11daa9026e172a6c8c5
Math_Caxa <70167858+wordanonimo@users.noreply.github.com>
2026-02-27 21:06:38 +0000

    Foi adicionado a Densidade e PIB per Capita
    Houve também alguns reajuste em algumas partes.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,8 @@ int main(){     //Criando as variaveis
     int     num_turistico1, num_turistico2; //Variaveis do ponto turistico
     float   area1, area2;                   //Variaveis da Area
     float   PIB1, PIB2;                     //Variaveis do PIB
+    float   PIBC1, PIBC2;                   //Variaveis do PIB per Capita
+    float   den1, den2;                     //Variaveis da Densidade Populacional
     char    estado1[20], estado2[20];       //Variaveis do nome do estado
     char    cod_carta1[20], cod_carta2[20]; //Variaveis do Código da carta
     char    nome1[20], nome2[20];           //Variaveis do Nome da Cidade
@@ -42,6 +44,11 @@ int main(){     //Criando as variaveis
     printf ("Ótimo!!! Tudo certo com as informações da sua primeira carta, agora, nos dê a informação da segunda carta\n");
     printf ("\n");
 
+    // Calculo da primeira cidade
+
+    den1   = (float) populacao1 / area1;  //Calculo da Densidade populacional
+    PIBC1  = (float) PIB1 / populacao1 ;  //Calculo do PIB per Capita
+
     // Coletando da Segunda carta 
     printf ("Digite o nome do Estado:\n");
     scanf  ("%s", &estado2);        // Nome do Estado
@@ -68,15 +75,22 @@ int main(){     //Criando as variaveis
     printf ("Tudo certo agora, aqui está a informação das duas cartas coletadas!!!\n");
     printf ("\n");
 
+    // Calculo da segunda cidade
+
+    den2   = (float) populacao2 / area2;  //Calculo da Densidade populacional
+    PIBC2  = (float) PIB2 / populacao2 ;  //Calculo do PIB per Capita
+
     // Mostrando as informações coletadas da primeira carta
     printf ("Primeira Carta\n");
     printf ("Estado: %s\n", estado1);
     printf ("Código da Carta: %s\n", cod_carta1);
     printf ("nome da Cidade: %s\n", nome1);
     printf ("População: %d\n", populacao1);
-    printf ("Área: %2.f km²\n", area1);
-    printf ("PIB: %2.f Bilhões de Reais\n", PIB1);
+    printf ("Área: %.2f km²\n", area1);
+    printf ("PIB: %.2f Bilhões de Reais\n", PIB1);
     printf ("Numêro de Pontos Turistico: %d\n", num_turistico1);
+    printf ("Densidade populacional: %.2f hab/km²\n", den1);
+    printf ("PIB per Capita: %.2f Reais\n", PIBC1);
     printf ("\n");
    
     // Mostrando as informações coletadas da segunda carta
@@ -85,7 +99,10 @@ int main(){     //Criando as variaveis
     printf ("Código da Carta: %s\n", cod_carta2);
     printf ("nome da Cidade: %s\n", nome2);
     printf ("População: %d\n", populacao2);
-    printf ("Área: %2.f km²\n", area2);
-    printf ("PIB: %2.f Bilhões de Reais\n", PIB2);
+    printf ("Área: %.2f km²\n", area2);
+    printf ("PIB: %.2f Bilhões de Reais\n", PIB2);
     printf ("Numêro de Pontos Turistico: %d\n", num_turistico2);
+    printf ("Densidade populacional: %.2f hab/km²\n", den2);
+    printf ("PIB per Capita: %.2f Reais\n", PIBC2);
+
 }   
